c80326858ef66549e649398c7db35970ec4a83ed
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2024-05-04 09:07:32 +0530

    rename `codeEditor.c.old` to `main.c`

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <gtk/gtk.h>
+
+static void app_activate (GApplication *app)
+{
+	GtkWidget *win;
+	GtkWidget *tv;
+	GtkTextBuffer *tb;
+	
+	GtkApplication *application = GTK_APPLICATION (app);
+	
+	// Adding File menu to the menubar
+	GMenu *menubar = g_menu_new ();
+	GMenuItem *menu_item_file = g_menu_item_new ("File", NULL);
+	// Creating a submenu
+	GMenu *menu = g_menu_new ();
+	GMenuItem *menu_item_save = g_menu_item_new ("Save", NULL);
+	g_menu_append_item (menu, menu_item_save);
+	g_object_unref (menu_item_save);
+	g_menu_item_set_submenu (menu_item_file, G_MENU_MODEL (menu));
+	g_object_unref (menu);
+	g_menu_append_item (menubar, menu_item_file);
+	g_object_unref (menu_item_file);
+	
+	gtk_application_set_menubar (GTK_APPLICATION (application), G_MENU_MODEL (menubar)); 
+	
+	win = gtk_application_window_new (GTK_APPLICATION (application));
+	gtk_window_set_title (GTK_WINDOW (win), "Code Editor");
+	gtk_window_set_default_size (GTK_WINDOW (win), 400, 300);
+	
+	tv = gtk_text_view_new ();
+	tb = gtk_text_view_get_buffer (GTK_TEXT_VIEW (tv));
+	gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (tv), GTK_WRAP_WORD_CHAR);
+	
+	gtk_window_set_child (GTK_WINDOW (win), tv);
+	
+	gtk_application_window_set_show_menubar (GTK_APPLICATION_WINDOW (win), TRUE);
+	gtk_window_present (GTK_WINDOW (win));
+}
+
+int main (int argc, char **argv)
+{
+	GtkApplication *app;
+	int stat;
+	
+	app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+	g_signal_connect (app, "activate", G_CALLBACK (app_activate), NULL);
+	stat = g_application_run (G_APPLICATION (app), argc, argv);
+	g_object_unref (app);
+	return stat;
+}
