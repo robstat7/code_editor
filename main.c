82a5e190f82082b6b7e9ae692e13b127a7dd384a
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2024-09-24 14:21:27 +0530

    show textview

--- main.c
+++ main.c
@@ -1,24 +1,27 @@
 #include <gtk/gtk.h>
 
-static void
-activate (GtkApplication *app,
-          gpointer        user_data)
+static void activate(GtkApplication *app, gpointer user_data)
 {
 	GtkWidget *win;
+	GtkWidget *tv;
+	GtkTextBuffer *tb;
 	GtkApplication *application;
 
 	application = GTK_APPLICATION (app);
 	win = gtk_application_window_new(GTK_APPLICATION (application));
 	gtk_window_set_title (GTK_WINDOW (win), "Code Editor");
 	gtk_window_set_default_size (GTK_WINDOW (win), 800, 600);
+
+	tv = gtk_text_view_new ();
+	tb = gtk_text_view_get_buffer (GTK_TEXT_VIEW (tv));
+	gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (tv), GTK_WRAP_WORD_CHAR);
+
+	gtk_container_add(GTK_CONTAINER(win), tv);
 	gtk_window_present (GTK_WINDOW (win));
+	gtk_widget_show_all(win);
 }
 
-
-
-int
-main (int    argc,
-      char **argv)
+int main(int argc, char **argv)
 {
   GtkApplication *app;
   int status;
